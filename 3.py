400c0f7b927b0196d220293abb21f73dc44c8395
Elizabeth <leezym@users.noreply.github.com>
2016-10-05 10:50:44 -0500

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,19 @@
+def Impar(tamaño):
+    c = 0
+    lista = []
+    lista2 = []
+    while (c < tamaño):
+        num = eval(input("Numero {}: ".format(c)))
+        lista.insert(c,num)
+        c = c+1
+    c = 0
+    x = 0
+    while (c < tamaño):
+        if (c % 2 != 0):
+            lista2.insert(x,lista[c])
+            x = x+1
+        c = c+1
+    return lista2            
+
+tamaño = int(input("De que tamaño desea la lista: "))
+print(Impar(tamaño))
